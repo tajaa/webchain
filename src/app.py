08f2bce97d590d2b3be4799595bcdc08070e0610
tajaa <tajatheprince@gmail.com>
2024-02-07 21:03:54 -0800

    added functionality to the sidebar and chatbox

--- src/app.py
+++ src/app.py
@@ -1,17 +1,46 @@
 import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+
+
+def get_response(user_input):
+    return "i don't know"
+
+
+# app.config
 
 st.set_page_config(page_title="Chat with websites", page_icon="🤖")
 
 st.title("Chat with websites")
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Hello, I'm a bot. How can I help you?")
+    ]
 
+# sidebar
 with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
 
-st.chat_input("type your message here...")
+if website_url is None or website_url == "":
+    st.info("please insert a website url")
 
-with st.chat_message("Ai"):
-    st.write("Hey, how can I help you")
+else:
+    # user input
+    user_query = st.chat_input("type message here...")
+    if user_query is not None and user_query != "":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
 
-with st.chat_message("Human"):
-    st.write("I want to know about langchain")
+# conversation
+for message in st.session_state.chat_history:
+
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.write(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.write(message.content)
+
+# with st.sidebar:
+#    st.write(st.session_state.chat_history)
